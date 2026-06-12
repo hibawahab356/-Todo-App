3dcd48c38fec5eb8aad9751be3070f9ae5be4ab0
hibawahab356 <ranihiba356@gmail.com>
2026-06-12 21:26:58 +0500

    Assigment

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-let addTask = document.getElementById("addtask")
-let task = document.getElementById("task")
-let list = document.getElementById("list")
+// let tasks = []
+// let addTask = () =>{
+//     let taskInput = document.getElementById("taskInput")
+//     console.log(taskInput)
+// }
 
-addTask.addEventListener("click",() =>{
-    console.log(task)
-
-
-    list.innerHTML += `<div class = "main" ><li>${task.value}</li> <div> <button onclick = "edit (this)">Edit </button> <button> Delete</button> </div></div>`
-    task.value = " "
-})
+// document.getElementById("newtask").addEventListener("click",() =>{
+//    e.preventDefault();
 
 
-function edit(element){
-   
-    let userTodo = element.parentNode.parentNode.firstChild;
-    task.value = userTodo.innerText
-
-    console.log(element.parentNode.parentNode.firstChild.innerText)
-    addTask.innerHTML = "Save"
-}
+//     addTask()
+// });
